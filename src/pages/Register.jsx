c3d364bf1108c2871e3fc846dd595575ecdf2651
maximeaaronn <maximeaaronn@gmail.com>
2026-09-20 15:12:07 +0100

    Update registration API URL to use environment variable

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Regsiter() {
         setUser('');
 
         try{
-            const response = await axios.post('http://127.0.0.1:8000/api/register', {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/register`, {
                 name,
                 email,
                 password,
